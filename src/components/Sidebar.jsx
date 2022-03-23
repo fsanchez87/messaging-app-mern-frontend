cd32b5fefa5554084345b7b5157e6f119a1de687
Francisco S <sanchezgarridof@gmail.com>
2022-03-23 23:02:10 +0100

    Creating a Sidebar Chats Componets

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 const Sidebar = () => {
   return (
@@ -30,7 +31,11 @@ const Sidebar = () => {
             <input placeholder="Search or start new chat" type="text"/>
           </div>
         </div>
-        <div className="sidebar__chats"></div>
+        <div className="sidebar__chats">
+          <SidebarChat/>
+          <SidebarChat/>
+          <SidebarChat/>
+        </div>
     </div>
   )
 }
