bae4b9ccf2172000177737f4d627edde502fb6ac
Francisco S <sanchezgarridof@gmail.com>
2022-03-23 08:56:01 +0100

    Creating a Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
-
+      <CssBaseline />
+      <div className="app__body">
+        <Sidebar/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Sidebar.css'
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <div className="sidebar__header">
+          <Avatar src="https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80"/>
+          <div className="sidebar__headerRight">
+            <IconButton>
+              <DonutLargeIcon/>
+            </IconButton>
+            <IconButton>
+              <ChatIcon/>
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon/>
+            </IconButton>
+          </div>
+        </div>
+        <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+            <SearchOutlinedIcon/>
+            <input placeholder="Search or start new chat" type="text"/>
+          </div>
+        </div>
+        <div className="sidebar__chats"></div>
+    </div>
+  )
+}
+
+export default Sidebar
