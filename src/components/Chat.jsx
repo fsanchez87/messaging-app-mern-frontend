e1b51e86b6da193fb8be35b4117b0ebcb8358104
Francisco S <sanchezgarridof@gmail.com>
2022-03-24 08:27:02 +0100

    Chat messages component

---
+++ src/components/Chat.jsx
@@ -0,0 +1,55 @@
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import AttachFile from "@mui/icons-material/AttachFile";
+import { Avatar, IconButton } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
+import MoreVert from "@mui/icons-material/MoreVert";
+
+const Chat = () => {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <div className="chat__message">
+          <span className="chat__name">Nabendu</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </div>
+        <div className="chat__message chat__receiver">
+          <span className="chat__name">Parag</span>
+          This is a message back
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </div>
+        <div className="chat__message">
+          <span className="chat__name">Nabendu</span>
+          This is a message again again
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </div>
+      </div>
+      <div className="chat__footer"></div>
+    </div>
+  );
+};
+
+export default Chat;
